51ee4165dfe0a184e7c58224a3d2f0080fa29788
Dark-Light-alt <1163743662@qq.com>
2020-08-27 18:59:20 +0800

    Chapter implement zhb

--- src/main/java/com/imooc/service/ChapterService.java
+++ src/main/java/com/imooc/service/ChapterService.java
@@ -1,13 +1,49 @@
 package com.imooc.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.imooc.entity.Chapter;
+import com.imooc.utils.common.Pages;
 
 public interface ChapterService extends IService<Chapter> {
     /**
      * 添加章节
+     *
      * @param chapter
      * @return
      */
     boolean append(Chapter chapter);
+
+    /**
+     * 根据课程 id 删除对应的所有章节和章节所对应的的视频
+     *
+     * @param courseId 章节 id
+     * @return
+     */
+    boolean removeCourseChapter(String courseId);
+
+    /**
+     * 修改章节信息
+     *
+     * @param chapter
+     * @return
+     */
+    boolean update(Chapter chapter);
+
+    /**
+     * 根据 id 查询章节信息
+     *
+     * @param chapterId 章节 id
+     * @return
+     */
+    Chapter findById(String chapterId);
+
+    /**
+     * 查询某个章节源的所有章节
+     *
+     * @param pages
+     * @param chapterResource 章节源：课程 id /专刊 id
+     * @return
+     */
+    Page<Chapter> findChapter(Pages pages, String chapterResource);
 }
--- src/main/java/com/imooc/service/impl/ChapterServiceImpl.java
+++ src/main/java/com/imooc/service/impl/ChapterServiceImpl.java
@@ -1,11 +1,14 @@
 package com.imooc.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.imooc.dao.ChapterDao;
 import com.imooc.entity.Chapter;
 import com.imooc.exception.ApiException;
 import com.imooc.service.ChapterService;
 import com.imooc.utils.common.CommonUtils;
+import com.imooc.utils.common.Pages;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,6 +25,68 @@ public class ChapterServiceImpl extends ServiceImpl<ChapterDao, Chapter> impleme
         vaild(chapter);
 
         return baseMapper.insert(chapter) != 0;
+    }
+
+    /**
+     * 根据课程 id 删除对应的所有章节和章节所对应的的视频
+     *
+     * @param courseId 章节 id
+     * @return
+     */
+    @Override
+    public boolean removeCourseChapter(String courseId) {
+        return false;
+    }
+
+    /**
+     * 修改章节信息
+     *
+     * @param chapter
+     * @return
+     */
+    @Override
+    public boolean update(Chapter chapter) {
+
+        vaild(chapter);
+
+        return baseMapper.updateById(chapter) != 0;
+    }
+
+    /**
+     * 根据 id 查询章节信息
+     *
+     * @param chapterId 章节 id
+     * @return
+     */
+    @Override
+    public Chapter findById(String chapterId) {
+        return baseMapper.selectById(chapterId);
+    }
+
+    /**
+     * 查询某个章节源的所有章节
+     *
+     * @param pages
+     * @param chapterResource 章节源：课程 id /专刊 id
+     * @return
+     */
+    @Override
+    public Page<Chapter> findChapter(Pages pages, String chapterResource) {
+
+        Page<Chapter> page = new Page<>(pages.getCurrentPage(), pages.getPageSize());
+
+        LambdaQueryWrapper<Chapter> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Chapter::getChapterResource, chapterResource);
+
+        String chapterName = pages.getSearchs().get("chapterName");
+
+        if (CommonUtils.isNotEmpty(chapterName)) {
+            wrapper.like(Chapter::getChapterName, chapterName);
+        }
+
+        wrapper.orderByAsc(Chapter::getCreateTime);
+
+        return baseMapper.selectPage(page, wrapper);
     }
 
     /**
