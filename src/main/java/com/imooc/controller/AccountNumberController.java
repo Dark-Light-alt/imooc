95a0bb55d3d702fa0141979ed84d6b4f13b3c24a
xinyuanGit <1979421537@qq.com>
2020-09-07 10:26:42 +0800

    fxy2

--- src/main/java/com/imooc/controller/AccountNumberController.java
+++ src/main/java/com/imooc/controller/AccountNumberController.java
@@ -23,17 +23,6 @@ public class AccountNumberController {
     @Resource
     private AccountNumberServiceImpl accountNumberServiceImpl;
 
-    @Resource
-    private SymmetryCryptoUtil symmetryCryptoUtil;
-
-    private static final String AUTHORITIES_KEY = "auth";
-
-    @Resource
-    JwtUtils jwtUtils;
-
-    // 签名秘钥
-    private String secretKey;
-
     @RequestMapping(value = "pagingFindAll", method = RequestMethod.POST)
     public Result pagingFindAll(@RequestBody Pages pages) {
 
@@ -76,53 +65,4 @@ public class AccountNumberController {
         return result;
     }
 
-    @RequestMapping(value = "changePassword", method = RequestMethod.POST)
-    public Result changePassword(@RequestBody Map<String, String> params) {
-
-        Result result = new Result();
-
-        // 获取员工 id
-        String employeeId = params.get("employeeId");
-
-        // 原密码
-        String password = params.get("password");
-
-        // 新密码
-        String newPassword = params.get("newPassword");
-
-        // 确定密码
-        String checkPassword = params.get("checkPassword");
-
-        if (!CommonUtils.isNotEmpty(password)) {
-            throw new ApiException(500, "原密码不能为空");
-        }
-
-        if (!CommonUtils.isNotEmpty(newPassword)) {
-            throw new ApiException(500, "新密码不能为空");
-        }
-
-        if (!CommonUtils.isNotEmpty(checkPassword)) {
-            throw new ApiException(500, "确认密码不能为空");
-        }
-
-        if (!newPassword.equals(checkPassword)) {
-            throw new ApiException(500, "两次密码不一致");
-        }
-
-        // 从数据库中查询到 账号信息
-        AccountNumber accountNumber = accountNumberServiceImpl.findByEmployeeId(employeeId);
-
-
-        if (!accountNumber.getPassword().equals(password)) {
-            throw new ApiException(500, "原密码错误");
-        }
-
-        accountNumber.setPassword(newPassword);
-
-        accountNumberServiceImpl.update(accountNumber);
-
-        result.success(200,"密码修改成功");
-
-        return result;
-    }
 }
