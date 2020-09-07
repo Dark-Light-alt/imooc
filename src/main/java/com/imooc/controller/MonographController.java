42e0ab597ea63d24da87c5ecc153c8c18f1783e2
Dark-Light-alt <1163743662@qq.com>
2020-09-07 18:42:59 +0800

    修改了许娜的专刊问题 zhb

--- src/main/java/com/imooc/controller/MonographController.java
+++ src/main/java/com/imooc/controller/MonographController.java
@@ -1,9 +1,7 @@
 package com.imooc.controller;
 
 import com.alibaba.fastjson.JSONObject;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.imooc.entity.Article;
 import com.imooc.entity.Monograph;
 import com.imooc.service.impl.MonographServiceImpl;
 import com.imooc.utils.aliyun.oss.FileStorageService;
@@ -24,47 +22,43 @@ import java.util.Map;
 public class MonographController {
 
     @Resource
-    FileStorageServiceImpl fileStorageServiceImpl;
+    private FileStorageServiceImpl fileStorageServiceImpl;
 
     @Resource
-    MonographServiceImpl monographServiceImpl;
+    private MonographServiceImpl monographServiceImpl;
 
     /**
      * 修改
+     *
      * @param monograph
      * @return
      */
-    @RequestMapping(value = "update",method = RequestMethod.PUT)
-    public Result update(@RequestBody Monograph monograph){
-        System.out.println("monograph:"+monograph);
+    @RequestMapping(value = "update", method = RequestMethod.PUT)
+    public Result update(@RequestBody Monograph monograph) {
 
         Result result = new Result();
 
-        boolean update = monographServiceImpl.update(monograph);
+        monographServiceImpl.update(monograph);
 
-
-        result.success(200,"操作成功");
-
+        result.success(200, "操作成功");
 
         return result;
     }
 
     /**
      * 修改
+     *
      * @param monograph
      * @return
      */
-    @RequestMapping(value = "putAway",method = RequestMethod.PUT)
-    public Result putAway(@RequestBody Monograph monograph){
-        System.out.println("monograph:"+monograph);
+    @RequestMapping(value = "putAway", method = RequestMethod.PUT)
+    public Result putAway(@RequestBody Monograph monograph) {
 
         Result result = new Result();
 
-        boolean update = monographServiceImpl.putAway(monograph);
+        monographServiceImpl.putAway(monograph);
 
-        if(update){
-            result.success(200,"上架成功");
-        }
+        result.success(200, "上架成功");
 
         return result;
     }
@@ -72,38 +66,41 @@ public class MonographController {
 
     /**
      * 根据monographId查询
+     *
      * @param monographId
      * @return
      */
-    @RequestMapping(value = "findById/{monographId}",method = RequestMethod.GET)
-    public Result findById(@PathVariable("monographId")String monographId){
+    @RequestMapping(value = "findById/{monographId}", method = RequestMethod.GET)
+    public Result findById(@PathVariable("monographId") String monographId) {
+
         Result result = new Result();
 
         Monograph monograph = monographServiceImpl.findById(monographId);
 
-        result.putData("monograph",monograph);
+        result.putData("monograph", monograph);
 
-        result.success(200,"SUCCESS");
+        result.success(200, "SUCCESS");
 
         return result;
     }
 
     /**
      * 修改专栏状态
+     *
      * @param map
      * @return
      */
-    @RequestMapping(value = "updateOffShelf",method = RequestMethod.POST)
-    public Result soldOut(@RequestBody Map map){
+    @RequestMapping(value = "updateOffShelf", method = RequestMethod.POST)
+    public Result soldOut(@RequestBody Map map) {
         String monographId = map.get("monographId").toString();
 
         int offShelf = Integer.parseInt(map.get("offShelf").toString());
         Result result = new Result();
 
-        boolean update = monographServiceImpl.updateOffShelf(monographId,offShelf);
+        boolean update = monographServiceImpl.updateOffShelf(monographId, offShelf);
 
-        if(update){
-            result.success(200,"操作成功");
+        if (update) {
+            result.success(200, "操作成功");
         }
 
         return result;
@@ -111,19 +108,20 @@ public class MonographController {
 
     /**
      * 添加
+     *
      * @param monograph
      * @return
      */
-    @RequestMapping(value = "append",method = RequestMethod.PUT)
-    public Result append(@RequestBody Monograph monograph){
+    @RequestMapping(value = "append", method = RequestMethod.PUT)
+    public Result append(@RequestBody Monograph monograph) {
         System.out.println(monograph);
 
         Result result = new Result();
 
         boolean append = monographServiceImpl.append(monograph);
 
-        if(append){
-            result.success(200,"操作成功");
+        if (append) {
+            result.success(200, "操作成功");
         }
 
         return result;
@@ -132,26 +130,26 @@ public class MonographController {
 
     /**
      * 分页关联查询专栏和作者
+     *
      * @param map
      * @return
      */
-    @RequestMapping(value = "pageFindMonographAuthor",method = RequestMethod.POST)
-    public Result pageFindMonographAuthor(@RequestBody Map map){
+    @RequestMapping(value = "pageFindMonographAuthor", method = RequestMethod.POST)
+    public Result pageFindMonographAuthor(@RequestBody Map map) {
         Result result = new Result();
 
-        Pages pages =  JSONObject.parseObject(map.get("pages").toString(),Pages.class);
+        Pages pages = JSONObject.parseObject(map.get("pages").toString(), Pages.class);
 
         Page<Monograph> data = null;
 
-        if(null != map.get("employeeId")){
+        if (null != map.get("employeeId")) {
             //如果员工编号不为null
             //查询员工的专刊
             String employeeId = map.get("employeeId").toString();
-            data = monographServiceImpl.findAllByEmployeeId(pages,employeeId);
+            data = monographServiceImpl.findAllByEmployeeId(pages, employeeId);
 
             System.out.println(data);
-        }else
-        {
+        } else {
             //根据完成状态分页查询
             data = monographServiceImpl.pageFindMonographAuthor(pages);
         }
@@ -161,27 +159,28 @@ public class MonographController {
         pages.setLastPage(data.getPages());
 
         result.setPages(pages);
-        result.putData("monographList",data.getRecords());
+        result.putData("monographList", data.getRecords());
 
-        result.success(200,"SUCCESS");
+        result.success(200, "SUCCESS");
 
         return result;
     }
 
     /**
      * 删除专栏
+     *
      * @param monographId
      * @return
      */
-    @RequestMapping(value = "delete/{monographId}",method = RequestMethod.GET)
-    public Result delete(@PathVariable("monographId") String monographId){
+    @RequestMapping(value = "delete/{monographId}", method = RequestMethod.GET)
+    public Result delete(@PathVariable("monographId") String monographId) {
 
         Result result = new Result();
 
         int i = monographServiceImpl.delete(monographId);
 
-        if(i>0){
-            result.success(200,"操作成功");
+        if (i > 0) {
+            result.success(200, "操作成功");
         }
 
         return result;
@@ -199,45 +198,47 @@ public class MonographController {
 
         String url = fileStorageServiceImpl.upload(inputStream, fileName, FileStorageService.IMG);
 
-        result.putData("url",url);
+        result.putData("url", url);
 
-        result.success(200,"SUCCESS");
+        result.success(200, "SUCCESS");
 
         return result;
     }
 
-    @RequestMapping(value = "previewMonograph",method = RequestMethod.POST)
-    public Result previewMonograph(@RequestBody Map map){
+    @RequestMapping(value = "previewMonograph", method = RequestMethod.POST)
+    public Result previewMonograph(@RequestBody Map map) {
+
         Result result = new Result();
 
-
         String monographId = map.get("monographId").toString();
-        System.out.println(monographId);
-        if(null != monographId){
+
+        if (null != monographId) {
+
             Monograph monograph = monographServiceImpl.previewMonograph(monographId);
 
-            result.putData("monograph",monograph);
+            result.putData("monograph", monograph);
         }
 
-        result.success(200,"SUCCESS");
+        result.success(200, "SUCCESS");
 
         return result;
     }
 
     /**
      * 根据条件查询所有上架专刊章节和试读文章
+     *
      * @return
      */
-    @RequestMapping(value = "listAllMonograph",method = RequestMethod.GET)
-    public Result listAllMonograph(){
+    @RequestMapping(value = "listAllMonograph", method = RequestMethod.GET)
+    public Result listAllMonograph() {
         Result result = new Result();
-
+        System.out.println("进来了");
         //查询所有试读的文章
-        List<Monograph> monographList = monographServiceImpl.listAllMonograph(null,2,1);
+        List<Monograph> monographList = monographServiceImpl.listAllMonograph(null, 2, 1);
 
-        result.putData("monographList",monographList);
+        result.putData("monographList", monographList);
 
-        result.success(200,"SUCCESS");
+        result.success(200, "SUCCESS");
 
         return result;
     }
@@ -245,10 +246,12 @@ public class MonographController {
 
     /**
      * 查询专刊下的章节文章
+     *
      * @return
      */
-    @RequestMapping(value = "listAllArticle/{articleId}",method = RequestMethod.GET)
-    public Result listAllArticle(@PathVariable("articleId") String articleId){
+    @RequestMapping(value = "listAllArticle/{articleId}", method = RequestMethod.GET)
+    public Result listAllArticle(@PathVariable("articleId") String articleId) {
+
         Result result = new Result();
 
         //根据文章编号查询专刊
@@ -256,15 +259,14 @@ public class MonographController {
         String monographId = monograph.getMonographId();
 
         //查询专刊下的所有章节和文章
-        List<Monograph> monographList = monographServiceImpl.listAllMonograph(monographId,2,null);
+        List<Monograph> monographList = monographServiceImpl.listAllMonograph(monographId, 2, null);
 
-        result.putData("monographList",monographList);
+        result.putData("monographList", monographList);
 
-        result.success(200,"SUCCESS");
+        result.success(200, "SUCCESS");
 
         return result;
     }
-
 
 
 }
--- src/main/java/com/imooc/utils/ReadAndWriteFile/ReadAndWriteFile.java
+++ src/main/java/com/imooc/utils/ReadAndWriteFile/ReadAndWriteFile.java
@@ -4,21 +4,22 @@ import com.imooc.exception.ApiException;
 
 import java.io.*;
 
-public class ReadAndWriteFile{
+public class ReadAndWriteFile {
 
     /**
      * 读取文件
+     *
      * @param filePath
      * @return
      * @throws Exception
      */
-    public static String readFile(String filePath){
-       /* //如果文件不存在
-        File file = new File("filePath");
-        System.out.println("filepath:"+filePath);
-        if(!file.exists()){
-            throw new ApiException(500,"文件损坏或者不存在");
-        }*/
+    public static String readFile(String filePath) {
+        //如果文件不存在
+        File file = new File(filePath);
+
+        if (!file.exists()) {
+            throw new ApiException(500, "文件损坏或者不存在");
+        }
 
         String str = "";
 
@@ -29,17 +30,16 @@ public class ReadAndWriteFile{
 
             int available = in.available();
 
-            if(available!=0){
+            if (available != 0) {
                 byte[] bytes = new byte[in.available()];
 
-                int len=-1;
-                while((len=in.read(bytes,0, bytes.length))!=-1){
-                    str=new String(bytes,0,len,"UTF-8");
+                int len = -1;
+                while ((len = in.read(bytes, 0, bytes.length)) != -1) {
+                    str = new String(bytes, 0, len, "UTF-8");
                 }
 
-            }else
-            {
-                str=" ";
+            } else {
+                str = " ";
             }
 
         } catch (FileNotFoundException e) {
@@ -48,30 +48,32 @@ public class ReadAndWriteFile{
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
                 in.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-
-        System.out.println("str:"+str);
-
-
-       return str;
+        return str;
     }
 
     /**
      * 写文件
+     *
      * @param str
      * @return
      */
-    public static int writeFile(String url,String str) {
+    public static int writeFile(String url, String str) throws IOException {
         //创建文件
         File file = new File(url);
 
+        file.getParentFile().mkdirs();
+
+        file.createNewFile();
+
         BufferedOutputStream out = null;
+
         try {
             out = new BufferedOutputStream(new FileOutputStream(file));
             byte[] bytes = str.getBytes("UTF-8");
@@ -84,7 +86,7 @@ public class ReadAndWriteFile{
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
                 out.flush();
                 out.close();
